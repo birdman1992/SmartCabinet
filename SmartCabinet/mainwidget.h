aec30dd7255ae565930bd49a9cfeadc65ff897f2
xian0gang <xian0gang@users.noreply.github.com>
2017-05-02 13:06:03 +0800

    Update mainwidget.h

--- SmartCabinet/mainwidget.h
+++ SmartCabinet/mainwidget.h
@@ -49,7 +49,7 @@ private:
     Ui::MainWidget *ui;
 //<<<<<<< HEAD
 
-    CabinetPanel cabinets[5];          //--定义5药柜
+//    CabinetPanel cabinets[5];          //--定义5药柜
 
     int num;                           //--药柜数目
     int lattice_num;                   //--药柜格子数目
@@ -60,7 +60,7 @@ private:
     QList<MedInf> medinf[5];           //--定义5个qlist，用于存储药柜信息
 //=======
     ControlDevice ctrlUi;//新添加的控制类，只需要和3个信号对接，详情见signals
-//    CabinetPanel cabinets[5];//--定义5药柜
+    CabinetPanel cabinets[5];//--定义5药柜
     QPushButton *btn_left; //--添加药柜按钮
     QPushButton *btn_add_lattice;
     QPushButton *btn_right;
