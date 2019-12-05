4e35e848a003aaa1d8e43cf19532c04c595eda67
birdman <871727949@qq.com>
2019-12-05 09:46:49 +0800

    back

--- SmartCabinet/Cabinet/cabinetwidget.cpp
+++ SmartCabinet/Cabinet/cabinetwidget.cpp
@@ -639,7 +639,6 @@ void CabinetWidget::recvScanData(QByteArray qba)
             qDebug()<<"[fetch]"<<"scan data not find";
             config->showMsg(MSG_GOODS_NOT_FIND, 1);
             win_access->showTips(MSG_GOODS_NOT_FIND, 1);
-//            win_access->fetchFailed(MSG_GOODS_NOT_FIND);
             return;
         }
         if(config->list_cabinet[addr.cabinetSeqNum]->list_case[addr.caseIndex]->list_goods[addr.goodsIndex]->num>0)//物品未取完
